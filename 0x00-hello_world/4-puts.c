6a2eb178fa3374a77646647654f0bb3ec5628bf0
ibrahimelfa <43937750+ibrahimelfa@users.noreply.github.com>
2022-10-17 18:51:33 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  * Description: 'C program that prints exactly "Programming 
  is like building a multilingual puzzle, followed by a new line'
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
