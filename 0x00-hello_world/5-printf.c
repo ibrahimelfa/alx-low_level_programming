8ac0956a076259ad5463b4c8951192ad836cc365
ibrahimelfa <43937750+ibrahimelfa@users.noreply.github.com>
2022-10-17 19:58:59 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
-#include stdio.h>
+#include <stdio.h>
 int main(void)
+/**
+ * main - Entry point
+ * Description: C program that uses printf.
+ * Return: Always 0 (Success)
+ */
 {
-	printf("with proper grammer\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
