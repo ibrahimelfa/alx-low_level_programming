1a3aa6a1a0a90bf1445cbb0b5129a8ab7c000047
ibrahimelfa <43937750+ibrahimelfa@users.noreply.github.com>
2022-10-18 06:52:39 +0200

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: C program that prints exactly Programming.
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void);
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	char l;
+	for (l = 'a' ; l < 'z' ;l++)
+		printf("%c", l);
+	printf("\n");
+}
