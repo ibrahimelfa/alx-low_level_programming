7576269d437c4ddbfb2482a2942a505168467966
ibrahimelfa <43937750+ibrahimelfa@users.noreply.github.com>
2022-10-18 03:39:25 +0200

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: C program that prints exactly Programming.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
