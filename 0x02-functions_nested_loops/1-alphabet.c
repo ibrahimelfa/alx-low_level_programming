f54706f15368843787b3553a5bbabc90d5c54135
ibrahimelfa <43937750+ibrahimelfa@users.noreply.github.com>
2022-10-18 07:02:38 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,14 +11,13 @@ int main(void)
 	print_alphabet();
 	return (0);
 }
-
+/**
+ * main - Entry point
+ * Description: C program that prints exactly Programming.
+ * Return: Always 0 (Success)
+ */
 void print_alphabet(void)
 {
-	/**
-	* main - Entry point
-	* Description: C program that prints exactly Programming.
-	* Return: Always 0 (Success)
-	*/
 	char l;
 
 	for (l = 'a' ; l < 'z' ; l++)
