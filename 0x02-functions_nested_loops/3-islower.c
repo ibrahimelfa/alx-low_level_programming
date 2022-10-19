a8b18b922e2dcb0865e2ebbd7a15cfbbef1f4760
ibrahimelfa <43937750+ibrahimelfa@users.noreply.github.com>
2022-10-19 04:14:30 +0200

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,6 +4,29 @@
  * Description: C program that prints exactly Programming.
  * Return: Always 0 (Success)
  */
+int _islower(int c)
+
 int main(void)
 {
+	int r;
+	
+	r = _islower('H');
+	putchar(r + '0');
+	r = _islower('o');
+	_putchar(r + '0');
+	r = _islower(108);
+	_putchar(r + '0');
+	_putchar('\n');
+}
+/**
+ * _islower - prints the alphabet .
+ */
+int _islower(int c)
+{
+	char l;
+
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 }
