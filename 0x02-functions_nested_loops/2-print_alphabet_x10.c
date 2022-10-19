6af2a466c50478fb58ddc9537caee6d85cd9aa44
ibrahimelfa <43937750+ibrahimelfa@users.noreply.github.com>
2022-10-19 03:31:42 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,6 +4,25 @@
  * Description: C program that prints exactly Programming.
  * Return: Always 0 (Success)
  */
+void print_alphabet(void);
+
 int main(void)
 {
+	print_alphabet_x10();
+	return (0);
+}
+/**
+ * print_alphabet - prints the alphabet .
+ */
+void print_alphabet_x10(void)
+{
+	char l;
+  int i;
+  
+  for (i = 0;i <= 10;i++)
+  {
+    for (l = 'A' ; l <= 'Z' ; l++)
+      putchar(l);
+    putchar('\n');
+  }
 }
