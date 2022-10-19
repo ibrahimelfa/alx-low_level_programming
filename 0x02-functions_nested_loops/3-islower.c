1fdf0597b9f9b82f19dbbd7518eef21ba3d618a0
ibrahimelfa <43937750+ibrahimelfa@users.noreply.github.com>
2022-10-19 04:25:24 +0200

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -11,12 +11,12 @@ int main(void)
 	int r;
 	
 	r = _islower('H');
-	putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
+	//putchar(r + '0');
+	//r = _islower('o');
+	//_putchar(r + '0');
+	//r = _islower(108);
+	//_putchar(r + '0');
+	//_putchar('\n');
 }
 /**
  * _islower - prints the alphabet .
